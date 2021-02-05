65122d2883d26d5209bae1441a6048c3cd581411
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-02-05 16:17:08 +0200

    Update Chair view

---
+++ Furniture/Furniture/TopHeaderView.swift
@@ -0,0 +1,20 @@
+//
+//  TopHeaderView.swift
+//  Furniture
+//
+//  Created by Cédric Bahirwe on 05/02/2021.
+//
+
+import SwiftUI
+
+struct TopHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TopHeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        TopHeaderView()
+    }
+}
