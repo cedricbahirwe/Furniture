19c946ac7d7aa1b5cf489ece859281491c4c5b73
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-02-05 15:37:15 +0200

    add Home screen and top tab bar

---
+++ Furniture/Furniture/Colors.swift
@@ -0,0 +1,8 @@
+//
+//  Colors.swift
+//  Furniture
+//
+//  Created by Cédric Bahirwe on 05/02/2021.
+//
+
+import Foundation
---
+++ Furniture/Furniture/ItemView.swift
@@ -0,0 +1,20 @@
+//
+//  ItemView.swift
+//  Furniture
+//
+//  Created by Cédric Bahirwe on 05/02/2021.
+//
+
+import SwiftUI
+
+struct ItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemView_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemView()
+    }
+}
